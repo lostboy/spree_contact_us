63f21c9b7826a5d00ac84e97067afd48d692f31e
lostboy <paul.crabtree@gmail.com>
2012-08-25 15:30:07 +0200

    default to not use captcha

--- app/models/captcha_configuration.rb
+++ app/models/captcha_configuration.rb
@@ -1,4 +1,4 @@
 # -*- encoding : utf-8 -*-
 class CaptchaConfiguration < Configuration
-  preference :use_captcha, :boolean, :default => true
+  preference :use_captcha, :boolean, :default => false
 end
